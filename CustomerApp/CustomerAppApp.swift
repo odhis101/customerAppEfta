6ae323e64a4b2f29f14df2bb8df0f88f7a46b531
odhis101 <joshodhiambo5@gmail.com>
2024-05-09 14:45:03 +0300

    Initial Commit

---
+++ CustomerApp/CustomerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomerAppApp.swift
+//  CustomerApp
+//
+//  Created by Natasha Odhiambo on 09/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
